a036e570c1e4b243e4753f0fc7b0d8bddaf1954d
George <gio.pircx@yahoo.com>
2021-04-18 21:47:57 +0400

    fix xenophobic issues in data

--- src/app/pages/product-data.service.ts
+++ src/app/pages/product-data.service.ts
@@ -5,39 +5,40 @@ import { Injectable } from '@angular/core';
 })
 export class ProductDataService {
   Data = [
-    {id:"#01",name:"giorgi",age:"4",hoby:"football"},
-    {id:"#07",name:"nika",age:"13",hoby:"footjob"},
-    {id:"#03",name:"nini",age:"12",hoby:"mamastan incesti"},
-    {id:"#02",name:"dato",age:"15",hoby:"hobi ara chemi yle"},
-    {id:"#08",name:"xamanwka",age:"23213",hoby:"yofna"},
-    {id:"#04",name:"mari",age:"7",hoby:"tvinis tyvna"},
-    {id:"#06",name:"lamara",age:"65",hoby:"pensiaze siaruli"},
-    {id:"#05",name:"bozi bozi",age:"13",hoby:"coliani kacebi"}
+    { id: "#01", name: "giorgi", age: "4", hoby: "football" },
+    { id: "#07", name: "nika", age: "13", hoby: "footjob" },
+    { id: "#03", name: "nini", age: "12", hoby: "mamastan incesti" },
+    { id: "#09", name: "nini", age: "12", hoby: "dedastan incesti" },
+    { id: "#02", name: "dato", age: "15", hoby: "hobi ara chemi yle" },
+    { id: "#08", name: "xamanwka", age: "23213", hoby: "yofna" },
+    { id: "#04", name: "mari", age: "7", hoby: "tvinis tyvna" },
+    { id: "#06", name: "lamara", age: "65", hoby: "pensiaze siaruli" },
+    { id: "#05", name: "bozi bozi", age: "13", hoby: "coliani kacebi" }
   ];
-  
+
   constructor() { }
-  getAll(){
+  getAll() {
     return this.Data;
   }
-  
-  getEach(id){
+
+  getEach(id) {
     for (let i = 0; i < this.Data.length; i++) {
-      if(this.Data[i].id == id)
+      if (this.Data[i].id == id)
         return this.Data[i];
     }
   }
-  
-  getByName(name){
+
+  getByName(name) {
     for (let i = 0; i < this.Data.length; i++) {
-      if(this.Data[i].name == name)
+      if (this.Data[i].name == name)
         return this.Data;
     }
   }
-  
-  getQuant(){
+
+  getQuant() {
     return this.Data.length;
   }
-  
-  OrderByAge(){}
-  OrderById(){}
+
+  OrderByAge() { }
+  OrderById() { }
 }
